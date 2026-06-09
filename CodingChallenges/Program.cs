4fa26306825a1451eb8d5f12c8a8afaa4d551ba2
James Carter <leonheartsquall69@yahoo.com>
2026-06-08 21:49:08 -0500

    Fixed my Project solution and Finished Day 2 challange

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,12 +1,15 @@
+﻿
+//lets you access namespaces from CodingChallenges
+using CodingChallenges.Arrays;
+using CodingChallenges.Strings;
 
 
 //FizzBuzz
-//lets you access Arrays from CodingChallenges
-using CodingChallenges.Arrays;
+
 //declares and int array fizzBuzzInput 
 //uses the Linq in c# to make a range 1 to 100 and cast that using ToArray 
  int[] fizzBuzzInput = Enumerable.Range(1,100).ToArray();
-
+ 
  //Declare an instance/object of FizzBuzz Class
  FizzBuzz fizzBuzz = new FizzBuzz();
 
@@ -14,4 +17,17 @@ using CodingChallenges.Arrays;
 //Uses the new FizzBuzz object we just created 
 //call the method/action availbe in the class
 //inputs the parameter for that type we said it requied in method creation
-FizzBuzz.PrintNumbers(fizzBuzzInput);
+fizzBuzz.PrintNumbers(fizzBuzzInput);
+
+
+//Reverse String 
+
+ //Declare and instance of ReverseString Class
+ ReverseString reverseString = new ReverseString();
+
+//A string to pass in to ReverseString
+ String aString = "Hello My Name is James Carter.";
+
+ string reversed = reverseString.ReverseProcess(aString);
+
+ System.Console.WriteLine(reversed);
--- CodingChallenges/Strings/ReverseString.cs
+++ CodingChallenges/Strings/ReverseString.cs
@@ -3,3 +3,25 @@
 // Example:
 // Input: "hello"
 // Output: "olleh"
+
+namespace CodingChallenges.Strings;
+
+
+public class ReverseString
+{
+    // The actions/methods need to return the same type in this case its a String 
+    public string ReverseProcess(string input)
+    {
+
+        string result = "";
+
+        // String can be index like an array
+        //input[i] returns the character at index i
+        for(int i = input.Length - 1; i >= 0 ; i--)
+        {
+            //Remember the [] here act as accessor for the value passed in at that index
+            result += input[i];
+        }
+        return result;
+    }
+}
